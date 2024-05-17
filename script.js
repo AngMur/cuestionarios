be2ca8419f9152b4fb2f2d80d86ad55c920b69e7
Angel Muratalla <163950319+AngMur@users.noreply.github.com>
2024-05-16 22:43:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,37 @@ function getQuestions(){
                 text: "¿Qué normativa se utiliza para garantizar la calidad del proceso de desarrollo de software enfocado en el diseño arquitectónico con realidad aumentada?",
                 answers: ["A) ISO 27000", "B) ISO 9001", "C) ISO 14971", "D) ISO 13407"],
                 correct: 1 // Índice de la respuesta correcta
-            }
+            },
+            {
+              text: "¿Qué tipo de tecnología desarrolla la empresa Mojo Vision?",
+              answers: ["A) Gafas de realidad aumentada", "B) Hologramas de realidad aumentada", "C) Aplicaciones con realidad aumentada", "D) Lentes de contacto con realidad aumentada"],
+              correct: 3 // Índice de la respuesta correcta
+          },
+          {
+              text: "¿Cómo se crean los hologramas?",
+              answers: ["A) Mediante la utilización de la luz", "B) Por medio de rayos láser", "C) Con múltiples espejos", "D) Mediante múltiples pantallas LED"],
+              correct: 0 // Índice de la respuesta correcta
+          },
+          {
+              text: "¿Cómo se pueden manejar los lentes de contacto AR?",
+              answers: ["A) Mediante gestos con las manos", "B) Mediante movimientos con los ojos", "C) Por comandos de voz", "D) A través del celular"],
+              correct: 1 // Índice de la respuesta correcta
+          },
+          {
+              text: "¿Quién fue el creador del primer objeto relacionado con la AR (el sensorama)?",
+              answers: ["A) Desarrollado por la universidad de Harvard", "B) Morton Heiling", "C) Ivan Sutherland", "D) Morton Leigh"],
+              correct: 1 // Índice de la respuesta correcta
+          },
+          {
+              text: "¿En qué consiste el HMD?",
+              answers: ["A) El primer simulador de aviación", "B) Un sistema de identificación", "C) Proyectaba imágenes en 3D, con una serie de sonidos, hacía vibrar el asiento del usuario y lanzaba aire", "D) Consistía en un casco que se colocaba en la cabeza del usuario con una pantalla montada a la altura de los ojos"],
+              correct: 3 // Índice de la respuesta correcta
+          },
+          {
+              text: "¿En qué aspecto el OIS (Sistema de identificación de objetos) sentó las bases para la AR?",
+              answers: ["A) Aplicando la superposición de gráficos en el mundo real", "B) Haciendo que la experiencia del simulador de aviación sea más realista", "C) Dando experiencias sensoriales al usuario mediante el movimiento del asiento, sonidos e imágenes", "D) Ninguna de las anteriores"],
+              correct: 0 // Índice de la respuesta correcta
+          }
         ];
     } else if (mainTitle.innerText == "Internet of Things") {
         questions = [
@@ -217,6 +247,60 @@ function getQuestions(){
           ];
           
         
+    } else if(mainTitle.innerText == "Red 5G"){
+      questions = [
+        {
+            text: "¿Cuántos Gb/s puede alcanzar el ancho de banda de la tecnología 5G?",
+            answers: ["A) 10 Gb/s", "B) 6 Gb/s", "C) 12 Gb/s", "D) 9 Gb/s"],
+            correct: 0 // Índice de la respuesta correcta
+        },
+        {
+            text: "¿Cuál fue el año en que se utilizó por primera vez la radio de dos vías portátil?",
+            answers: ["A) 1922", "B) 1925", "C) 1923", "D) 1929"],
+            correct: 2 // Índice de la respuesta correcta
+        },
+        {
+            text: "¿Cómo se llama el equipo militar que Motorola creó en la Segunda Guerra Mundial?",
+            answers: ["A) Handie Talkie H19-45", "B) Handie Talkie H12-16", "C) Walkie Talkie H12-16", "D) Walkie Talkie H19-45"],
+            correct: 1 // Índice de la respuesta correcta
+        },
+        {
+            text: "¿Qué fue lo que caracterizó a la tecnología 1G?",
+            answers: ["A) Hacer que los teléfonos móviles tengan acceso a internet.", "B) Que tuviera un sistema totalmente digital.", "C) Un menor tiempo de latencia.", "D) Ser analógica y estrictamente para voz."],
+            correct: 3 // Índice de la respuesta correcta
+        },
+        {
+            text: "¿Cuál fue el primer protocolo de codificación que se utilizó para los teléfonos móviles de la 2G?",
+            answers: ["A) GSM (Global System For Mobile Communication)", "B) CDMA (Code Division Multiplex Access)", "C) GPRS (General Packet Radio Service)", "D) TDMA (Time Division Multiplex Access)"],
+            correct: 0 // Índice de la respuesta correcta
+        },
+        {
+            text: "¿Cuál fue el año en que tuvo su aparición la tecnología 3G?",
+            answers: ["A) 2002", "B) 2003", "C) 2001", "D) 2000"],
+            correct: 2 // Índice de la respuesta correcta
+        },
+        {
+            text: "¿Cuál es la característica principal de la tecnología 4G?",
+            answers: ["A) Que puede lograr una velocidad de hasta 300 Mb/s.", "B) Que dispone de una mayor resistencia a interferencias.", "C) Su altísima tasa de transmisión.", "D) El paso de dispositivos analógicos a un sistema totalmente digital."],
+            correct: 2 // Índice de la respuesta correcta
+        },
+        {
+            text: "¿Cuál es la norma ISO que establece un conjunto de indicadores que permiten medir y evaluar el desempeño de las ciudades en términos de sostenibilidad?",
+            answers: ["A) ISO 20000", "B) ISO 9001", "C) ISO 27033", "D) ISO 37120"],
+            correct: 3 // Índice de la respuesta correcta
+        },
+        {
+            text: "¿Cómo se llama la empresa que trabaja con servicios de ambulancias a través de telesalud para ayudar a los servicios de emergencia a proporcionar cuidados más completos a quienes acaban de sufrir ictus graves?",
+            answers: ["A) Visionable", "B) MiuMiu", "C) LG", "D) Samsung"],
+            correct: 0 // Índice de la respuesta correcta
+        },
+        {
+            text: "¿Cuál es el año en que Corea del Sur hará los primeros proyectos piloto de 6G?",
+            answers: ["A) 2028", "B) 2026", "C) 2025", "D) 2027"],
+            correct: 1 // Índice de la respuesta correcta
+        }
+    ];
+    
     }
 
     return questions;
